7c8fa946dd0d562b5c3cd1d8f91a9f8ee2b69096
weehaa <vzg1000@mail.ru>
2021-04-28 18:33:26 +0400

    refactor input with chakra-ui

--- components/Cart/CartItem.tsx
+++ components/Cart/CartItem.tsx
@@ -6,6 +6,13 @@ import {IProduct} from '../../interfaces/products'
 
 import {Button} from '@chakra-ui/button'
 import {Td, Tr} from '@chakra-ui/table'
+import {
+    NumberDecrementStepper,
+    NumberIncrementStepper,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper
+} from '@chakra-ui/number-input'
 
 interface ICartItemProps extends IProduct{
     price: number
@@ -28,12 +35,18 @@ const CartItem = ({id, name, count, price, cartCount}: ICartItemProps) => {
         <Tr>
             <Td>{name}</Td>
             <Td>
-                <input
-                    type="number"
+                <NumberInput
+                    onChange={onInputChange}
                     value={cartCount}
-                    onChange={({target: {value}}) =>
-                        onInputChange(value)}
-                />
+                    max={count}
+                    bgColor="white"
+                >
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
                 <div className={count === cartCount ? 'warn' : 'hide'}>Количество ограничено</div>
             </Td>
             <Td className='price-cell' isNumeric><b>{price} руб.</b>/шт.</Td>
